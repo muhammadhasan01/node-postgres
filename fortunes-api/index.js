8343a6698d41d9396a1d5d26629c4f045a0eae0f
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-01-31 16:50:48 +0700

    feat: add post method and overwrite

--- fortunes-api/index.js
+++ fortunes-api/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const fortunes = require('./data/fortunes.json');
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.get('/fortunes', (req, res) => {
   res.json(fortunes);
@@ -12,12 +16,22 @@ app.get('/fortunes/random', (req, res) => {
   const randomIdx = Math.floor(Math.random() * fortunes.length);
   const randomFortune = fortunes[randomIdx];
   res.json(randomFortune);
-})
+});
 
 app.get('/fortunes/:id', (req, res) => {
   const {id} = req.params;
   const fortune = fortunes.find(f => f.id === Number(id));
   res.json(fortune);
+});
+
+app.post('/fortunes', (req, res) => {
+  const {message, luckyNumber, spiritAnimal} = req.body;
+  const fortuneIds = fortunes.map(f => f.id);
+  const fortuneId = fortuneIds.length > 0 ? Math.max(...fortuneIds) + 1 : 0;
+  const fortune = {id: fortuneId, message, luckyNumber, spiritAnimal};
+  const newFortunes = fortunes.concat(fortune);
+  fs.writeFile('./data/fortunes.json', JSON.stringify(newFortunes), err => console.log(err));
+  res.send(newFortunes);
 })
 
 app.listen(port, () => {
