63f1edabbb41dc788ca192c92c601f3dae8c9987
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-01-31 16:30:49 +0700

    chore: add fortunes random endpoint

--- fortunes-api/index.js
+++ fortunes-api/index.js
@@ -8,6 +8,12 @@ app.get('/fortunes', (req, res) => {
   res.json(fortunes);
 });
 
+app.get('/fortunes/random', (req, res) => {
+  const randomIdx = Math.floor(Math.random() * fortunes.length);
+  const randomFortune = fortunes[randomIdx];
+  res.json(randomFortune);
+})
+
 app.listen(port, () => {
   console.log(`Listening on Port ${port}`);
 });
